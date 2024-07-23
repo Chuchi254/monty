43a7d08cd59de4ffe6aa8314b59b7f9648c832ff
Chuchi254 <samsongikonyo142@gmail.com>
2024-07-22 23:28:35 -0700

    prints the value at the top of the stack

---
+++ pint.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "monty.h"
+
+/**
+ * pint - Prints the value at the top of the stack.
+ * @stack: double pointer to the stack
+ * @line_number: the current line number in the file
+ */
+
+void pint(stack_t **stack, unsigned int line_number)
+{
+	if (stack == NULL || *stack == NULL)
+	{
+		fprintf(stderr, "L%u: cant' pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	printf("%d\n", (*stack)->n);
+}
